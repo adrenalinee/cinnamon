80881336214578d91e4d252b5a8ffb4a8a40b459
sds <dsshin@daihansci@co.kr>
2016-01-29 18:52:35 +0900

    마테리알 웹 콘솔 개발중

---
+++ cinnamon-core-web-material/src/main/groovy/org/cinnamon/web/configuration/restController/UserBaseRestController.groovy
@@ -0,0 +1,44 @@
+package org.cinnamon.web.configuration.restController
+
+import org.cinnamon.core.domain.UserBase
+import org.cinnamon.core.service.UserBaseService;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * 
+ *
+ * created date: 2015. 9. 9.
+ * @author 신동성
+ */
+@RestController
+@RequestMapping("/rest/configuration/users")
+class UserBaseRestController {
+	Logger logger = LoggerFactory.getLogger(getClass())
+	
+	@Autowired
+	UserBaseService<UserBase> userService
+	
+	
+	@RequestMapping(value="", method=RequestMethod.GET)
+	Page<UserBase> users(Pageable pageable) {
+		logger.info("String")
+		
+		return userService.getList(pageable)
+	}
+	
+	
+	@RequestMapping(value="{userId}", method=RequestMethod.GET)
+	UserBase users(@PathVariable String userId) {
+		logger.info("String")
+		
+		return userService.get(userId)
+	}
+}
--- cinnamon-core-web-material/src/main/resources/static/js/configuration/users/listController.js
+++ cinnamon-core-web-material/src/main/resources/static/js/configuration/users/listController.js
@@ -2,10 +2,14 @@ angular.module('cinnamon')
 .controller('configuration.users.list', function($scope, $http) {
 	console.log('configuration.users.list');
 	
+	$scope.selectedUsers = [];
+	
 	$http.get('/rest/configuration/users')
 	.success(function(data) {
 		console.log(data);
 		
 		$scope.domains = data;
 	});
+	
+	
 });
