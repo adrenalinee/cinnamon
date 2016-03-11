0a8d6351e0015c80f17b05dd4f42c6342401420d
sds <dsshin@daihansci@co.kr>
2016-03-11 17:29:27 +0900

    세션을 통해서 개인 정보를 전달하는 api 추가 웹사이트 해더에 동적 메뉴 구성할 수 있도록 수정 angular-material 버전업(1.0.6)

--- cinnamon-core-web-material/src/main/groovy/org/cinnamon/web/configuration/restController/SessionRestController.groovy
+++ cinnamon-core-web-material/src/main/groovy/org/cinnamon/web/configuration/restController/SessionRestController.groovy
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController("cinnamon.sessionRestController")
 @RequestMapping("/rest/session")
 class SessionRestController {
 	Logger logger = LoggerFactory.getLogger(getClass())
