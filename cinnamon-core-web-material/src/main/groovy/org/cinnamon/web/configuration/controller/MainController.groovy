91935ed400fa42397efd51bd0d280aab5ad8acbc
sds <dsshin@daihansci@co.kr>
2016-04-01 18:03:46 +0900

    메인 화면에서 첫페이지 찾는 코드 에러 수정

--- cinnamon-core-web-material/src/main/groovy/org/cinnamon/web/configuration/controller/MainController.groovy
+++ cinnamon-core-web-material/src/main/groovy/org/cinnamon/web/configuration/controller/MainController.groovy
@@ -7,6 +7,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping
 
 /**
@@ -31,7 +32,7 @@ class MainController {
 		logger.info("start")
 //		"redirect:/configuration"
 		
-		if (defaultPage != null) {
+		if (!StringUtils.isEmpty(defaultPage)) {
 			return defaultPage
 		}
 		
