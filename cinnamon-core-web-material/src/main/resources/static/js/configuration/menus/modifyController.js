8ac17b70d6f8f9238005088987d8d5ea78b7a68f
dsshin <adrenalinee31@gmail.com>
2016-04-12 23:39:10 +0900

    충돌해결

--- cinnamon-core-web-material/src/main/resources/static/js/configuration/menus/modifyController.js
+++ cinnamon-core-web-material/src/main/resources/static/js/configuration/menus/modifyController.js
@@ -1,10 +1,22 @@
 angular.module('cinnamon')
-.controller('configuration.menus.modify', function($scope, $http, $stateParams, $mdDialog, $mdMedia) {
+.controller('configuration.menus.modify', function($scope, $http, $stateParams, $mdDialog, $mdMedia, message, pageMove, $state) {
 	console.log('configuration.menus.modify');
 	
-	
 	var menuId = $stateParams.menuId;
+	
 	$scope.menuId = menuId;
+	
+	$http.get('/rest/configuration/groups/menuTypes/childs')
+	.success(function(data) {
+		$scope.menuTypes = data;
+	});
+	
+	$http.get('/rest/configuration/groups/menuPositions/childs')
+	.success(function(data) {
+		$scope.menuPositions = data;
+	});
+	
+	
 	
 	$scope.load = function() {
 			$http.get('/rest/configuration/menus/' + menuId)
@@ -15,4 +27,19 @@ angular.module('cinnamon')
 	}
 		
 	$scope.load();
+
+	// 정보 수정
+	$scope.modify = function() {
+		var params = angular.copy($scope.menu);
+		$http.put('/rest/configuration/menus/' + menuId , params)
+			.success(function(result) {
+				message.alert('수정되었습니다.');
+				pageMove.go('view', {menuId : menuId});
+		})
+	}
+	
+	// 리스트로 이동
+	$scope.goBack = function() {
+		pageMove.go('view' , {menuId : menuId});
+	}
 });
