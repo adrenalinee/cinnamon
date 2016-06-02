ca367476081fa3484b6a2092aae6f4f74acf2cd5
audtjddld <audtjddld@daihan-biomedical.com>
2016-06-02 09:17:34 +0900

    angular moment 라이브러리 추가

--- cinnamon-core-web-material/src/main/resources/static/js/cinnamon.js
+++ cinnamon-core-web-material/src/main/resources/static/js/cinnamon.js
@@ -14,7 +14,9 @@ angular.module(
 		'ui.bootstrap.pagination',
 		'ui.bootstrap.pager', 
 		'ngFileUpload', 
-		'mdPickers' ])
+		'mdPickers',
+		'angularMoment'
+		])
 
 /*
  * .factory('defaultErrorInterceptor', function($q) { return { 'responseError':
