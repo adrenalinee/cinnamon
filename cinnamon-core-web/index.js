4fe134b5f02b589f8d7e2d4661ba45f269f8d93f
dsshin <adrenalinee31@gmail.com>
2017-01-09 00:32:18 +0900

    angular-material이 로딩이 제대로 안되던 문제 수정

--- cinnamon-core-web/index.js
+++ cinnamon-core-web/index.js
@@ -1,22 +1,25 @@
 var angular = require('angular');
 require('angular-animate');
 require('angular-aria');
-require('angular-material');
-require('angular-material/angular-material.css');
 require('angular-messages');
 require('angular-sanitize');
+
 require('angular-ui-router');
+
+require('angular-material');
+require('angular-material/angular-material.css');
+
 require('material-steppers');
 require('material-steppers/dist/material-steppers.css');
 
-//angular.module('cinnamon.core', [
-//	'mdSteppers',
-//	'ngAnimate',
-//	'ngAria',
-//	'ngMessages',
-//	'ngMaterial',
-//	'ngSanitize', 
-//	'ui.router']);
+angular.module('cinnamon.core', [
+	'mdSteppers',
+	'ngAnimate',
+	'ngAria',
+	'ngMessages',
+	'ngMaterial',
+	'ngSanitize', 
+	'ui.router']);
 
 require('./dist/cinnamon.core.bundle');
 require('./dist/cinnamon.core.css');
